f855c7d304667daf49d7578aa353b81ae9a46215
Hamza Anver <hamzaanver1234@gmail.com>
2024-08-19 16:43:42 +0530

    doxy

--- include/version.h
+++ include/version.h
@@ -4,15 +4,15 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 22
-#define VERSION_GIT_HASH "3ee2c82"
+#define VERSION_PATCH 23
+#define VERSION_GIT_HASH "d79d072"
 #define VERSION_GIT_BRANCH "main"
 #define VERSION_BUILD_DATE "2024-08-19"
-#define VERSION_BUILD_TIME "15:59:58"
+#define VERSION_BUILD_TIME "16:43:09"
 
 #define VERSION_ENVIRONMENT "nb-iot-nodemcu-32s-4mb"
 
-#define VERSION_STRING "v1.0.22 Date: 2024-08-19 Time: 15:59:58 Git: main_3ee2c82"
-#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.22-20240819-155958-main-3ee2c82"
+#define VERSION_STRING "v1.0.23 Date: 2024-08-19 Time: 16:43:09 Git: main_d79d072"
+#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.23-20240819-164309-main-d79d072"
 
 #endif // VERSION_H
