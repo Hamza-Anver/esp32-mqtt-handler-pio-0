1849771af39c37525e7ecab2dd746adbc0403594
Hamza Anver <hamzaanver1234@gmail.com>
2024-08-23 01:29:39 +0530

    built to update version and ota json

--- include/version.h
+++ include/version.h
@@ -4,15 +4,15 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 30
-#define VERSION_GIT_HASH "c687ce3"
+#define VERSION_PATCH 32
+#define VERSION_GIT_HASH "a4c4e4e"
 #define VERSION_GIT_BRANCH "main"
 #define VERSION_BUILD_DATE "2024-08-23"
-#define VERSION_BUILD_TIME "01:20:50"
+#define VERSION_BUILD_TIME "01:29:29"
 
 #define VERSION_ENVIRONMENT "nb-iot-nodemcu-32s-4mb"
 
-#define VERSION_STRING "v1.0.30 Date: 2024-08-23 Time: 01:20:50 Git: main_c687ce3"
-#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.30-20240823-012050-main-c687ce3"
+#define VERSION_STRING "v1.0.32 Date: 2024-08-23 Time: 01:29:29 Git: main_a4c4e4e"
+#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.32-20240823-012929-main-a4c4e4e"
 
 #endif // VERSION_H
