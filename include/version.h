f709a9f040d0476525d976ca6e099794f705a3af
Hamza Anver <hamzaanver1234@gmail.com>
2024-08-22 18:04:25 +0530

    example bin file allocation

--- include/version.h
+++ include/version.h
@@ -4,15 +4,15 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 26
-#define VERSION_GIT_HASH "6a1bdae"
+#define VERSION_PATCH 27
+#define VERSION_GIT_HASH "dd0bb92"
 #define VERSION_GIT_BRANCH "main"
 #define VERSION_BUILD_DATE "2024-08-22"
-#define VERSION_BUILD_TIME "17:51:37"
+#define VERSION_BUILD_TIME "18:03:10"
 
 #define VERSION_ENVIRONMENT "nb-iot-nodemcu-32s-4mb"
 
-#define VERSION_STRING "v1.0.26 Date: 2024-08-22 Time: 17:51:37 Git: main_6a1bdae"
-#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.26-20240822-175137-main-6a1bdae"
+#define VERSION_STRING "v1.0.27 Date: 2024-08-22 Time: 18:03:10 Git: main_dd0bb92"
+#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.27-20240822-180310-main-dd0bb92"
 
 #endif // VERSION_H
--- scripts/bincreate.py
+++ scripts/bincreate.py
@@ -12,7 +12,8 @@ def extract_definitions(header_file):
     definitions = {}
     with open(header_file, 'r') as file:
         for line in file:
-            match = re.match(r'#define\s+(\w+)\s+"(.+)"', line)
+            match = re.match(r'#define\s+(\w+)\s+"?([^"\s]+)"?', line)
+
             if match:
                 key, value = match.groups()
                 definitions[key] = value
@@ -29,8 +30,9 @@ def after_build(source, target, env):
     version_defs = extract_definitions(version_header)
 
     # Get the destination path
-    dest_name = version_defs["VERSION_STRING_NO_SPACES"] + ".bin"
-    newbin_dir = os.path.join(env["PROJECT_DIR"],"binfiles", version_defs["nb-iot-nodemcu-32s-4mb"]) # type: ignore
+    dest_name = version_defs["VERSION_ENVIRONMENT"] + ".bin"
+    newbin_dir = os.path.join(env["PROJECT_DIR"],"binfiles", version_defs["VERSION_ENVIRONMENT"]) # type: ignore
+    
     if not os.path.exists(newbin_dir):
         os.makedirs(newbin_dir)
     else:
@@ -38,17 +40,23 @@ def after_build(source, target, env):
         shutil.rmtree(newbin_dir)
         os.makedirs(newbin_dir)
 
-    copy_bin_to_loc(binpath, os.path.join(env["PROJECT_DIR"],"binfiles",dest_name)) # type: ignore
+    copy_bin_to_loc(binpath, os.path.join(newbin_dir,dest_name)) # type: ignore
 
-    fileurl = "https://github.com/iot-bits/nb-iot-nodemcu-32s-4mb/releases/download/" + version_defs["VERSION_STRING"] + "/" + dest_name
+    binurl = "https://github.com/Hamza-Anver/esp32-mqtt-handler-pio-0/" + version_defs["VERSION_ENVIRONMENT"] + '/' + dest_name
 
     jsondata = {
         "name": version_defs["VERSION_STRING"],
         "major": version_defs["VERSION_MAJOR"],
         "minor": version_defs["VERSION_MINOR"],
         "patch": version_defs["VERSION_PATCH"],
-        "bin": dest_name
+        "env": version_defs["VERSION_ENVIRONMENT"],
+        "binurl": binurl
     }
+
+    newjson_loc = os.path.join(env["PROJECT_DIR"],"binfiles", version_defs["VERSION_ENVIRONMENT"], "ota.json") # type: ignore
+    
+    with open(newjson_loc, 'w') as jsonfile:
+        json.dump(jsondata, jsonfile, indent=4)
 
 
 Import("env") # type: ignore
