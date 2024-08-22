dd0bb9205b3fa4a8646e77d60ca1827552e2e4ec
Hamza Anver <hamzaanver1234@gmail.com>
2024-08-22 17:51:47 +0530

    new repo

--- include/version.h
+++ include/version.h
@@ -4,15 +4,15 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 25
-#define VERSION_GIT_HASH "f855c7d"
+#define VERSION_PATCH 26
+#define VERSION_GIT_HASH "6a1bdae"
 #define VERSION_GIT_BRANCH "main"
 #define VERSION_BUILD_DATE "2024-08-22"
-#define VERSION_BUILD_TIME "17:38:06"
+#define VERSION_BUILD_TIME "17:51:37"
 
 #define VERSION_ENVIRONMENT "nb-iot-nodemcu-32s-4mb"
 
-#define VERSION_STRING "v1.0.25 Date: 2024-08-22 Time: 17:38:06 Git: main_f855c7d"
-#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.25-20240822-173806-main-f855c7d"
+#define VERSION_STRING "v1.0.26 Date: 2024-08-22 Time: 17:51:37 Git: main_6a1bdae"
+#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.26-20240822-175137-main-6a1bdae"
 
 #endif // VERSION_H
