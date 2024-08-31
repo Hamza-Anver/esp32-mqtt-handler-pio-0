993d08b756a67ceb7a8b3c4377584a90b0c302d1
Hamza Anver <hamzaanver1234@gmail.com>
2024-08-31 15:09:55 -0400

    fixed pdf link

--- include/version.h
+++ include/version.h
@@ -4,15 +4,15 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 36
-#define VERSION_GIT_HASH "36e79fe"
+#define VERSION_PATCH 40
+#define VERSION_GIT_HASH "306f0bc"
 #define VERSION_GIT_BRANCH "main"
-#define VERSION_BUILD_DATE "2024-08-27"
-#define VERSION_BUILD_TIME "18:38:30"
+#define VERSION_BUILD_DATE "2024-08-31"
+#define VERSION_BUILD_TIME "15:09:45"
 
 #define VERSION_ENVIRONMENT "nb-iot-nodemcu-32s-4mb"
 
-#define VERSION_STRING "v1.0.36 Date: 2024-08-27 Time: 18:38:30 Git: main_36e79fe"
-#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.36-20240827-183830-main-36e79fe"
+#define VERSION_STRING "v1.0.40 Date: 2024-08-31 Time: 15:09:45 Git: main_306f0bc"
+#define VERSION_STRING_NO_SPACES "nb-iot-nodemcu-32s-4mb-v1.0.40-20240831-150945-main-306f0bc"
 
 #endif // VERSION_H
